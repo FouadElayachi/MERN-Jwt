f141a0c7799a2b60c1bb39144e721532db9c16ce
fouadGeek <elayachifouad@gmail.com>
2020-09-29 20:49:57 +0100

    Modify attribute class by className in footer components

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,23 +3,23 @@ import { Link } from 'react-router-dom';
 
 export default function Footer() {
     return (
-        <footer class="page-footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col l6 s12">
-                        <h5 class="white-text">Footer Content</h5>
-                        <p class="grey-text text-lighten-4">Astronomy and learning lovers</p>
+        <footer className="page-footer">
+            <div className="container">
+                <div className="row">
+                    <div className="col l6 s12">
+                        <h5 className="white-text">Footer Content</h5>
+                        <p className="grey-text text-lighten-4">Astronomy and learning lovers</p>
                     </div>
-                    <div class="col l4 offset-l2 s12">
-                        <h5 class="white-text">Links</h5>
+                    <div className="col l4 offset-l2 s12">
+                        <h5 className="white-text">Links</h5>
                         <ul>
-                            <li><Link class="grey-text text-lighten-3" to="/">Home</Link></li>
+                            <li><Link className="grey-text text-lighten-3" to="/">Home</Link></li>
                         </ul>
                     </div>
                 </div>
             </div>
-            <div class="footer-copyright">
-                <div class="container">
+            <div className="footer-copyright">
+                <div className="container">
                     © 2020 dropeditions
                 </div>
             </div>
